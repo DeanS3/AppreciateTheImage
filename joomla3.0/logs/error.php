536f795635bd7ee24fd923ee4dc5d0fdbc79e54f
Dean Stallworth <d_stallworth3@yahoo.com>
2013-04-13 20:12:50 -0500

    Joomla 3.0
    Added joomla CMS to site.

---
+++ joomla3.0/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-04-13 22:41:33 UTC
+#Software: Joomla Platform 12.2.0 Stable [ Neil Armstrong ] 21-September-2012 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2013-04-13T22:41:33+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2013-04-13T22:41:47+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2013-04-13T22:42:00+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
